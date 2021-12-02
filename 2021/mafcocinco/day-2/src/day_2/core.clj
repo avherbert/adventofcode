5dc1f14d80d19d1b64a17c39e7a209c861816223
Marcus Oladell <1581372+MafcoCinco@users.noreply.github.com>
2021-12-02 11:10:22 -0600

    Improved version

--- 2021/mafcocinco/day-2/src/day_2/core.clj
+++ 2021/mafcocinco/day-2/src/day_2/core.clj
@@ -1,46 +1,45 @@
 (ns day-2.core
   (:require [clojure.string :as string]))
 
-(def position {:horizontal 0 :depth 0 :aim 0})
-
-(defn move-part-1
-  "Moves specified direction using rules from part 1"
-  [position [direction amount]]
-  (let [amount (Integer/parseInt amount)]                ;; convert string to int
-    (case (keyword direction)                            ;; convert direction to keyword for use in case form
-      :up (update position :depth - amount)              ;; `up` case, subtract `amount` from `depth`
-      :down (update position :depth + amount)            ;; `down` case, add `amount` to `depth`
-      :forward (update position :horizontal + amount)))) ;; `forward` case, add `amount` to `horizontal`
+(def position )
 
 (defn forward
   "Handles `forward` command for part 2"
-  [{:keys [aim] :as position} amount]           ;; Destructure position, binding `aim` value
-  (-> (update position :depth + (* aim amount)) ;; add `aim` * `amount` to `depth`
-      (update :horizontal + amount)))           ;; add `amount` to `horizontal`
+  [{:keys [aim] :as position} amount]            ;; Destructure position, binding `aim` value
+  (cond-> (update position :horizontal + amount) ;; add `amount` to `horizontal`
+    (int? aim)                                   ;; When `aim` is an integer...
+    (update :depth + (* aim amount))))           ;; ... update `depth` with `aim` * `amount` 
 
-(defn move-part-2
-  "Moves specified direction using rules from part 2"
+(defn depth
+  "Conditionally handles depth adjustments"
+  [{:keys [aim] :as position} direction-fn amount] ;; Destructure position, binding `aim` value
+  (if (int? aim)                                   ;; If `aim` is an integer...
+    (update position :aim direction-fn amount)     ;; ... adjust the value of `aim`
+    (update position :depth direction-fn amount))) ;; ... otherwise adjust the value of `depth`
+
+(defn move
+  "Moves specified direction using rules from part 1"
   [position [direction amount]]
   (let [amount (Integer/parseInt amount)]   ;; convert string to int
     (case (keyword direction)               ;; convert direction to keyword for use in case form
-      :up (update position :aim - amount)   ;; `up` case, subtract `amount` from `depth`
-      :down (update position :aim + amount) ;; `down` case, add `amount` to `depth`
-      :forward (forward position amount)))) ;; apply `forward` function
+      :up (depth position - amount)         ;; adjust `depth` down by `amount`
+      :down (depth position + amount)       ;; adjust `depth` up by `amount`
+      :forward (forward position amount)))) ;; adjust `forward` by `amount`
 
-(defn calculate-answer
+(defn final-position
   "Calculates the answer"
   [position]
   (->> (select-keys position [:horizontal :depth]) ;; Extract `horizontal` and `depth` for calculation
        (vals)                                      ;; Get `vals` from `position` map
        (apply *)))                                 ;; Apply multiplication to each int in sequence
 
-(defn calculate
+(defn navigate
   "Reads data from the data file, transforming it into a usable state"
-  [move-fn]
-  (let [data (-> (slurp "./resources/puzzle_input.dat") (string/split #"\n"))] 
+  [position]
+  (let [data (-> (slurp "./resources/puzzle_input.dat") (string/split #"\n"))]
     (->> (map #(string/split % #" ") data) ;; String -> 2-tuple
-         (reduce move-fn position)         ;; Reduce across data using `move-fn`, starting at `position`
-         (calculate-answer))))             ;; Calculate the answer
+         (reduce move position)            ;; Reduce across data using `move-fn`, starting at `position`
+         (final-position))))               ;; Calculate the answer
 
-(defn part-1 [] (calculate move-part-1))
-(defn part-2 [] (calculate move-part-2))
+(defn part-1 [] (navigate {:horizontal 0 :depth 0}))
+(defn part-2 [] (navigate {:horizontal 0 :depth 0 :aim 0}))
