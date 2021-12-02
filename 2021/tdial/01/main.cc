667f19335931afd473566331c9a84472b0c2e15f
Thomas R. Dial <dialtr@gmail.com>
2021-12-01 22:49:34 -0500

    ran clang format

--- 2021/tdial/01/main.cc
+++ 2021/tdial/01/main.cc
@@ -10,46 +10,46 @@ using ::std::string;
 
 // Read next valid depth.
 bool next_depth(ifstream& stream, int* depth) {
-	if (stream.eof()) {
-		return false;
-	}
+  if (stream.eof()) {
+    return false;
+  }
 
-	string token;
-	getline(stream, token);
-	if (token.size() == 0) {
-		// getline returning empty string for last line
-		return false;
-	}
+  string token;
+  getline(stream, token);
+  if (token.size() == 0) {
+    // getline returning empty string for last line
+    return false;
+  }
 
   *depth = atoi(token.c_str());
-	return true;
+  return true;
 }
 
 int main(int argc, char* argv[]) {
-	// Open depth file.
-	ifstream file;
-	file.open("input.txt");
-	
-	// Prime by reading first depth value.
-	int prev = 0;
-	if (!next_depth(file, &prev)) {
-		cerr << "failed to read first depth." << endl;
-		return 1;
-	}
+  // Open depth file.
+  ifstream file;
+  file.open("input.txt");
 
-	// Track count of depths largest than the previous measurement.
-	int larger_than_previous = 0;
+  // Prime by reading first depth value.
+  int prev = 0;
+  if (!next_depth(file, &prev)) {
+    cerr << "failed to read first depth." << endl;
+    return 1;
+  }
 
-	// Read all valid entries
-	int next = 0;
-	while (next_depth(file, &next)) {
-		if (next > prev) {
-			++larger_than_previous;
-		}
-		prev = next;
-	}
+  // Track count of depths largest than the previous measurement.
+  int larger_than_previous = 0;
 
-	cout << "Answer: " << larger_than_previous << endl;
+  // Read all valid entries
+  int next = 0;
+  while (next_depth(file, &next)) {
+    if (next > prev) {
+      ++larger_than_previous;
+    }
+    prev = next;
+  }
 
-	return 0;
+  cout << "Answer: " << larger_than_previous << endl;
+
+  return 0;
 }
