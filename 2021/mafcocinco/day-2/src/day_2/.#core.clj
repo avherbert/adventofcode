aaf560219c8c6f29a168faea85725c5bcb8c6e04
Marcus Oladell <1581372+MafcoCinco@users.noreply.github.com>
2021-12-02 10:34:25 -0600

    Solution for day 2

---
+++ 2021/mafcocinco/day-2/src/day_2/.#core.clj
@@ -0,0 +1,1 @@
+marcusoladell@Marcuss-MacBook-Pro.local.29252
---
+++ 2021/mafcocinco/day-2/src/day_2/core.clj
@@ -0,0 +1,46 @@
+(ns day-2.core
+  (:require [clojure.string :as string]))
+
+(def position {:horizontal 0 :depth 0 :aim 0})
+
+(defn move-part-1
+  "Moves specified direction using rules from part 1"
+  [position [direction amount]]
+  (let [amount (Integer/parseInt amount)]                ;; convert string to int
+    (case (keyword direction)                            ;; convert direction to keyword for use in case form
+      :up (update position :depth - amount)              ;; `up` case, subtract `amount` from `depth`
+      :down (update position :depth + amount)            ;; `down` case, add `amount` to `depth`
+      :forward (update position :horizontal + amount)))) ;; `forward` case, add `amount` to `horizontal`
+
+(defn forward
+  "Handles `forward` command for part 2"
+  [{:keys [aim] :as position} amount]           ;; Destructor position, binding `aim` value
+  (-> (update position :depth + (* aim amount)) ;; add `aim` * `amount` to `depth`
+      (update :horizontal + amount)))           ;; add `amount` to `horizontal`
+
+(defn move-part-2
+  "Moves specified direction using rules from part 2"
+  [position [direction amount]]
+  (let [amount (Integer/parseInt amount)]   ;; convert string to int
+    (case (keyword direction)               ;; convert direction to keyword for use in case form
+      :up (update position :aim - amount)   ;; `up` case, subtract `amount` from `depth`
+      :down (update position :aim + amount) ;; `down` case, add `amount` to `depth`
+      :forward (forward position amount)))) ;; apply `forward` function
+
+(defn calculate-answer
+  "Calculates the answer"
+  [position]
+  (->> (select-keys position [:horizontal :depth])
+       (vals)
+       (apply *)))
+
+(defn calculate
+  "Reads data from the data file, transforming it into a usable state"
+  [move-fn]
+  (let [data (-> (slurp "./resources/puzzle_input.dat") (string/split #"\n"))]
+    (->> (map #(string/split % #" ") data)
+         (reduce move-fn position)
+         (calculate-answer))))
+
+(defn part-1 [] (calculate move-part-1))
+(defn part-2 [] (calculate move-part-2))
